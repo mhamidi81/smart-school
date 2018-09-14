a64e2bccdac007386007d2ce374918e0603efdd2
mhamidi81 <mohamed.hamidi@gmail.com>
2018-09-15 00:06:19 +0100

    add splash screen and its icon

---
+++ app/src/main/java/ma/aiaexpert/free/smartschool/SplashActivity.java
@@ -0,0 +1,17 @@
+package ma.aiaexpert.free.smartschool;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Start home activity
+        startActivity(new Intent(SplashActivity.this, HomeActivity.class));
+        // close splash activity
+        finish();
+    }
+}
